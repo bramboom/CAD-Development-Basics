bbdea1e45f300c165306295c64a53fdef810a26c
bramboom <bramboom1@yandex.ru>
2022-01-23 21:49:08 +0700

    правки

--- src/GUI/MainForm.cs
+++ src/GUI/MainForm.cs
@@ -189,7 +189,7 @@ namespace GUI
         private void buttonBuild_Click(object sender, EventArgs e)
         {
             // todo: const or use bool +
-            bool isAnError = true;
+            bool isAnError = false;
             string errorMessage = "Параметры введены неверно:";
             foreach (var pair in _dictionary)
             {
@@ -198,7 +198,7 @@ namespace GUI
                     pair.Key.Text == "")
                 {
                     errorMessage += pair.Value.Value;
-                    isAnError = false;
+                    isAnError = true;
                 }
             }
 
